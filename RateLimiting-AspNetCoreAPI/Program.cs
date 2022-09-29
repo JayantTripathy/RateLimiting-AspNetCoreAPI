f3247682508957ad6b753a2da9e9960c899d663e
jayant <jayanttripathy1985@gmail.com>
2022-09-29 23:33:02 +0530

    Add project files.

---
+++ RateLimiting-AspNetCoreAPI/Program.cs
@@ -0,0 +1,43 @@
+using AspNetCoreRateLimit;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//// Services add for rate limiting
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddMemoryCache();
+
+//// Services add for rate limiting
+builder.Services.Configure<IpRateLimitOptions>(builder.Configuration.GetSection("IpRateLimiting"));
+builder.Services.AddSingleton<IIpPolicyStore, MemoryCacheIpPolicyStore>();
+builder.Services.AddSingleton<IRateLimitCounterStore, MemoryCacheRateLimitCounterStore>();
+builder.Services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+builder.Services.AddSingleton<IProcessingStrategy, AsyncKeyLockProcessingStrategy>();
+builder.Services.AddInMemoryRateLimiting();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+//// Middleware Services use for rate limiting
+app.UseIpRateLimiting();
+
+app.MapControllers();
+
+app.Run();
